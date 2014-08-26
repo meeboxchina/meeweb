61526aee6adf7552908cf02dea1419f7cde5b27d
Sunny.Sun <sunyu.cn@gmail.com>
2014-08-26 21:50:17 +0800

    user

---
+++ src/cn/meebox/web/Login.java
@@ -0,0 +1,59 @@
+package cn.meebox.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Login
+ */
+@WebServlet("/user")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Login() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		
+		User user = new User(username, password);
+		
+		String auth = user.Auth();
+		
+		PrintWriter out = response.getWriter();
+		out.print(auth);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		
+		User user = new User(username, password);
+		
+		String auth = user.Auth();
+		
+		PrintWriter out = response.getWriter();
+		out.print(auth);
+	}
+
+}
---
+++ src/cn/meebox/web/User.java
@@ -0,0 +1,53 @@
+package cn.meebox.web;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class User {
+	private String username;
+	private String password;
+	
+	public User(String username, String password) {
+		// TODO Auto-generated constructor stub
+		this.username = username;
+		this.password = password;
+	}
+	
+	public String Auth() {
+		String auth = "";
+		
+		Connection conn = null; //定义一个MYSQL链接对象
+		try {
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/meebox", "meebox", "meebox");
+			Statement stmt = conn.createStatement();
+			String sql = "select * from user where username=\"" + username + "\" and password=\"" + password + "\"";
+			ResultSet rs = stmt.executeQuery(sql);
+			
+			if(rs.next()){
+				auth = "ok";
+			}else{
+				auth = "error";
+			}
+		        
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return auth;
+	}
+
+}
