fdaa7a1d6989d3a197c3cd70d9d62b5d1b4c1040
Sunny.Sun <sunyu.cn@gmail.com>
2014-08-27 00:46:41 +0800

    login

--- src/cn/meebox/web/Login.java
+++ src/cn/meebox/web/Login.java
@@ -10,6 +10,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 /**
  * Servlet implementation class Login
  */
@@ -29,21 +32,29 @@ public class Login extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		String username = request.getParameter("username");
 		String password = request.getParameter("password");
 		
 		User user = new User(username, password);
 		String auth = user.auth();
-		
-		HttpSession session = request.getSession();
-		session.setAttribute("user", username);
-		String sessionId = session.getId();
-		
-		
 		PrintWriter out = response.getWriter();
-		out.print(sessionId);
-		out.print(auth);
+		
+		
+		JSONObject json=new JSONObject();  
+	    JSONArray jsonMembers = new JSONArray();  
+	    JSONObject member1 = new JSONObject();  
+	    
+	    if(auth=="ok"){
+	    	member1.put("state", "true");  
+		    member1.put("goto", "http://meebox.cn/getfile");  
+		    member1.put("errorcode","404");  
+		    member1.put("errormsg", "密码错误");  
+	    }
+	    
+	    jsonMembers.put(member1);  
+	    
+		out.print(jsonMembers.toString());
+		out.close();
 		
 	}
 
@@ -58,8 +69,22 @@ public class Login extends HttpServlet {
 		User user = new User(username, password);
 		String auth = user.auth();
 		PrintWriter out = response.getWriter();
-		out.print(auth);
-		out.print("OK");
+		
+		
+		JSONObject json=new JSONObject();  
+	    JSONArray jsonMembers = new JSONArray();  
+	    JSONObject member1 = new JSONObject();  
+	    
+	    if(auth=="ok"){
+	    	member1.put("state", "true");  
+		    member1.put("goto", "http://meebox.cn/getfile");  
+		    member1.put("errorcode","404");  
+		    member1.put("errormsg", "密码错误");  
+	    }
+	    
+	    jsonMembers.put(member1);  
+	    
+		out.print(jsonMembers.toString());
 		out.close();
 	}
 
