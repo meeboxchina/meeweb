a7abf9f875ff05e1812050295b1dbc549b9dfa55
Sunny.Sun <sunyu.cn@gmail.com>
2014-08-28 03:15:12 +0800

    file

---
+++ src/cn/meebox/web/File.java
@@ -0,0 +1,82 @@
+package cn.meebox.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class File
+ */
+@WebServlet("/File")
+public class File extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public File() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String filename = "";
+		HttpSession session = request.getSession();
+		String username=(String)session.getAttribute("username"); 
+		if(username!=""){
+			Connection conn = null; //定义一个MYSQL链接对象
+			try {
+				Class.forName("com.mysql.jdbc.Driver").newInstance();
+				conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/meebox", "meebox", "meebox");
+				Statement stmt = conn.createStatement();
+				String sql = "select * from file where username=\"" + username + "\"";
+				ResultSet rs = stmt.executeQuery(sql);
+				
+				if(rs.next()){
+					filename = rs.getString("filename");
+				}else{
+					
+				}
+				
+				PrintWriter out = response.getWriter();
+				out.print(filename);
+			        
+			} catch (InstantiationException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IllegalAccessException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
