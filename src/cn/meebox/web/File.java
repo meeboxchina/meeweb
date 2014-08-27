8ee2ce71ba30cf8942c5ac616108dd386f27ce3f
Sunny.Sun <sunyu.cn@gmail.com>
2014-08-28 03:35:25 +0800

    file

--- src/cn/meebox/web/File.java
+++ src/cn/meebox/web/File.java
@@ -18,7 +18,7 @@ import javax.servlet.http.HttpSession;
 /**
  * Servlet implementation class File
  */
-@WebServlet("/File")
+@WebServlet("/file")
 public class File extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
